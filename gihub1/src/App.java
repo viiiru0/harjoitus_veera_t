dcfe956fd02e4cb5fb2ac7c7ed75f2fe0b118d2f
viiiru0 <veera.tepponen@hotmail.com>
2025-09-13 22:43:36 +0300

    Update App.java

--- gihub1/src/App.java
+++ gihub1/src/App.java
@@ -1,21 +1,37 @@
 import java.util.Random;
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Random random = new Random ();
-        Scanner in = new Scanner(System.in);
-        int randomNumber1 = random.nextInt(11);
-        int randomNumber2 = random.nextInt(11);
-        int randomNumber3 = random.nextInt(11);
-        System.out.println(randomNumber1);
-        System.out.println(randomNumber2);
-        System.out.println(randomNumber3);
-            if(randomNumber1 == 7 || randomNumber2 == 7 || randomNumber3 == 7) {
-                System.out.println("Voitit pelin.");
+        int euros = 5;
+
+        while(euros > 0) {
+            int randomNumber1 = random.nextInt(11);
+            int randomNumber2 = random.nextInt(11);
+            int randomNumber3 = random.nextInt(11);
+            
+            euros--;
+            System.out.println("Arvotut numerot: ");
+            System.out.println(randomNumber1);
+            System.out.println(randomNumber2);
+            System.out.println(randomNumber3);
+            int sevens = 0;
+            if (randomNumber1 == 7) sevens++;
+            if (randomNumber2 == 7) sevens++;
+            if (randomNumber3 == 7) sevens++;
+                
+            if (sevens == 1) {
+                euros += 3;
+                System.out.println("Voitit 3 euroa.");
+            } else if (sevens == 2) {
+                euros += 5;
+                System.out.println("Voitit 5 euroa.");
+            } else if (sevens == 3) {
+                euros += 6;
+                System.out.println("Voitit 6 euroa.");
             }
-            else {
-                    System.out.println("Hävisit pelin.");
-                }
+        }
+        System.out.println("Peli on päättynyt.");
         }
     }
+
